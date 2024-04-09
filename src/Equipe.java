7b0d6482fdc058ceaf5d9c7e9c4d6c585bbd8825
El Mellouki <amin.el-mellouki@etu.univ-orleans.fr>
2024-04-09 15:24:39 +0200

    a

---
+++ src/Equipe.java
@@ -0,0 +1,66 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Equipe{
+    // private Pays p = null;
+    private String nomEquipe;
+    private List<Athlete> lesAthletes;
+    //private List<Epreuve> lesEpreuves;
+
+
+    public Equipe(String nomEquipe) {
+        this.nomEquipe = nomEquipe;
+        this.lesAthletes = new ArrayList<>();
+    }
+
+    // public Pays getPays(){
+    //     return this.p
+    // }
+
+    public List<Athlete> getAthletes(){
+        return this.lesAthletes;
+    }
+
+    // public List<Epreuve> getEpreuves(){
+    //     return this.lesEpreuves;
+    // }
+
+    public Athlete plusFort() {
+        Athlete athletePlusFort = null;
+        // utilisation de la valeur minimale pour un entier en java pour initialiser le maximum
+        int maxForce = Integer.MIN_VALUE;        
+        for (Athlete a : this.lesAthletes) {
+            if (a.getForce() > maxForce) {
+                maxForce = a.getForce();
+                athletePlusFort = a;
+            }
+        }
+        return athletePlusFort;
+    }
+
+    public Athlete plusEndurant() {
+        Athlete athletePlusEndurant = null;
+        // utilisation de la valeur minimale pour un entier en java pour initialiser le maximum
+        int maxEndurance = Integer.MIN_VALUE;
+        for (Athlete a : this.lesAthletes) {
+            if (a.getEndurance() > maxEndurance) {
+                maxEndurance = a.getEndurance();
+                athletePlusEndurant = a;
+            }
+        }
+        return athletePlusEndurant;
+    }
+    
+    public Athlete plusAgile() {
+        Athlete athletePlusAgile = null;
+        // utilisation de la valeur minimale pour un entier en java pour initialiser le maximum
+        int maxAgilite = Integer.MIN_VALUE;
+        for (Athlete a : this.lesAthletes) {
+            if (a.getAgilite() > maxAgilite) {
+                maxAgilite = a.getAgilite();
+                athletePlusAgile = a;
+            }
+        }
+        return athletePlusAgile;
+    }    
+}
---
+++ src/TestsAthlete.java
@@ -0,0 +1,7 @@
+public class TestsAthlete {
+    public static void main(String[] args) {
+        Athlete a1 = new Athlete("Dupont", "Jean", "M","Français", 10, 4, 4);
+        System.out.println(a1.toString());
+        a1.setAgilite(8);
+    }
+}
