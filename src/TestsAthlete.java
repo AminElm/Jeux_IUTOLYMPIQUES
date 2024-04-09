a8cb0cc6e5a6cbb2c6a3df37b003bb118a4ce407
El Mellouki <amin.el-mellouki@etu.univ-orleans.fr>
2024-04-09 15:30:33 +0200

    Création des dossiers bin, src et doc + création des classes Athlete, Equipe et TestsAthlete

--- src/TestsAthlete.java
+++
@@ -1,7 +0,0 @@
-public class TestsAthlete {
-    public static void main(String[] args) {
-        Athlete a1 = new Athlete("Dupont", "Jean", "M","Français", 10, 4, 4);
-        System.out.println(a1.toString());
-        a1.setAgilite(8);
-    }
-}
