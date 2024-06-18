f583c0ee5b9424650afd32eff87f2653a62501d2
manglij <nicolas.lepage41@gmail.com>
2024-06-18 14:44:51 +0200

    redebut de la base de donnee

--- src/main/java/com/cdal/DonnerJo.java
+++ src/main/java/com/cdal/DonnerJo.java
@@ -82,9 +82,15 @@ public class DonnerJo {
             e.ajouteParticipant(a);
             p.ajouterParticipant(a);
 
-                    
+            Statement st=laConnexion.createStatement();
 
+            for 
 
+            String ath = "insert into DETUDIANT(nume,nome,prenome,idf) values("+String.valueOf(e.getNum())+","+e.getNom()+","+e.getPrenom()+","+String.valueOf(e.getFormation())+ ");";
+            System.out.println(etu);
+            st.executeUpdate(etu);
+    
+        }
     }
    
 }
