583f994eaa70efa8f93f1b798893e7f3c26980fa
manglij <nicolas.lepage41@gmail.com>
2024-06-18 15:03:32 +0200

    les requetes

---
+++ src/main/java/com/cdal/requete.java
@@ -0,0 +1,11 @@
+import java.sql.*;
+import java.util.ArrayList;
+
+
+public class requete {
+	ConnexionMySQL laConnexion;
+	Statement st;
+
+	requete(ConnexionMySQL laConnexion) {
+		this.laConnexion = laConnexion;
+	}}
