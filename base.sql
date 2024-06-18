e56b7baf1e08370ccd15d27ec3ebf72b4aa23129
manglij <nicolas.lepage41@gmail.com>
2024-06-18 15:56:53 +0200

    requetes role

--- base.sql
+++ base.sql
@@ -70,17 +70,9 @@ ALTER TABLE PARTICIPE_A ADD FOREIGN KEY (idEp) REFERENCES EPREUVE (idEp);
 ALTER TABLE PARTICIPE_Eq ADD FOREIGN KEY (idEq) REFERENCES EQUIPE (idEq);
 ALTER TABLE PARTICIPE_Eq ADD FOREIGN KEY (idEp) REFERENCES EPREUVE (idEp);
 
-
-
-create role administrateur , organisateur, visiteur;
-grant all on BD.* to admin;
-grant insert , update, delete, select on BD.base to organisateur;
-grant select on BD.base to visiteur;
-
 CREATE TABLE PERSONNE{
-  idPers int,
-  prenomA varchar(30),
-  nomA varchar(30),
+  pseuos varchar(30),
   mdp varchar(30),
-  PRIMARY KEY (idpers)
+  roles varchar(30),
+  PRIMARY KEY (pseuos)
 }
--- src/main/java/com/cdal/requete.java
+++ src/main/java/com/cdal/requete.java
@@ -21,6 +21,7 @@ public class requete {
 					+String.valueOf(idA)+","+String.valueOf(idP)+","+String.valueOf(idEq)+","+prenom+","+nom+","+sexe+","+String.valueOf(endurence)+","+String.valueOf(forces)+","+String.valueOf(agilite)+");");
 
 		sn.executeUpdate();
+		return this.getNbAthlete();
 			
 	}
 
@@ -32,18 +33,26 @@ public class requete {
 					+String.valueOf(idA)+","+String.valueOf(idP)+","+"NULL"+","+prenom+","+nom+","+sexe+","+String.valueOf(endurence)+","+String.valueOf(forces)+","+String.valueOf(agilite)+");");
 
 		sn.executeUpdate();
+		return this.getNbAthlete();
 			
 	}
-	public int getidPays(Pays p){
+	public int getidPays(Pays p)throws SQLException{
 		st = laConnexion.createStatement();
 		ResultSet rs = st.executeQuery("select idP from PAYS where nomP ="+p.getNom());
 		rs.next();
 		return rs.getInt(1);
 	}
 
-	public int getidEquipe(Equipe e){
+	public int getidEquipe(Equipe e)throws SQLException{
 		st = laConnexion.createStatement();
 		ResultSet rs = st.executeQuery("select idEq from EQUIPE where nomEq ="+e.getNom());
+		rs.next();
+		return rs.getInt(1);
+	}
+
+	public int getNbAthlete()throws SQLException{
+		st = laConnexion.createStatement();
+		ResultSet rs = st.executeQuery("select count(idA) from ATHLETE;");
 		rs.next();
 		return rs.getInt(1);
 	}
@@ -55,12 +64,22 @@ public class requete {
 
 
 
-	public void ajoutevisiteur(String nom, String prenom, String mdp){
+	public void ajoutevisiteur(String pseudos, String mdp)throws SQLException{
 		PreparedStatement sn = laConnexion.prepareStatement(
-					"insert into PERSONNE (idPers, prenomP, nomA, mdp) values ("+nom+","+prenom+","+mdp+",);");
+			"insert into PERSONNE (pseudos, mdp,roles) values ("+pseudos+","+mdp+"visit);");
 		sn.executeUpdate();
+	}
 
-	
+	public void ajouteadmin(String pseudos, String mdp)throws SQLException{
+		PreparedStatement sn = laConnexion.prepareStatement(
+			"insert into PERSONNE (pseudos, mdp,roles) values ("+pseudos+","+mdp+"admin);");
+		sn.executeUpdate();
+	}
+
+	public void ajouteorga(String pseudos, String mdp)throws SQLException{
+		PreparedStatement sn = laConnexion.prepareStatement(
+			"insert into PERSONNE (pseudos, mdp,roles) values ("+pseudos+","+mdp+"orga);");
+		sn.executeUpdate();
 	}
 
 
