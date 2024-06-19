8308b76104874fbadd67af725d0c434111616f72
sham MOHAMED MURSITH <145010597+sham-mohamed-mursith@users.noreply.github.com>
2024-06-19 16:12:40 +0200

    Update ControleurAjouterAthlete.java

--- src/main/java/com/cdal/ControleurAjouterAthlete.java
+++ src/main/java/com/cdal/ControleurAjouterAthlete.java
@@ -1,8 +1,11 @@
+package main.java.com.cdal;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
 public class ControleurAjouterAthlete implements EventHandler<ActionEvent> {
     private FenetreAdministrateur app;
+
 
     public ControleurAjouterAthlete( FenetreAdministrateur app){
         this.app=app;
@@ -10,8 +13,7 @@ public class ControleurAjouterAthlete implements EventHandler<ActionEvent> {
 
     @Override
     public void handle(ActionEvent event) {
-        app.modeAjouterAthlete();
-        app.activerBoutonMaison();
+        app.getApp().afficherFentreAjouterAthlete();
     }
 
     
