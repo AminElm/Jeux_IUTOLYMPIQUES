4060347bf9dde485136c3d0020413053fbe1f5bf
sham MOHAMED MURSITH <145010597+sham-mohamed-mursith@users.noreply.github.com>
2024-06-20 16:33:33 +0200

    Update and rename ControleurAjouterAthlete.java to ControleurAccederFenetreAjouterAthlete.java

--- src/main/java/com/cdal/ControleurAccederFenetreAjouterAthlete.java
+++ src/main/java/com/cdal/ControleurAccederFenetreAjouterAthlete.java
@@ -3,17 +3,17 @@ package main.java.com.cdal;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
-public class ControleurAjouterAthlete implements EventHandler<ActionEvent> {
+public class ControleurAccederFenetreAjouterAthlete implements EventHandler<ActionEvent> {
     private FenetreAdministrateur app;
 
 
-    public ControleurAjouterAthlete( FenetreAdministrateur app){
+    public ControleurAccederFenetreAjouterAthlete( FenetreAdministrateur app){
         this.app=app;
     }
 
     @Override
     public void handle(ActionEvent event) {
-        app.getApplication().afficherFentreAjouterAthlete();
+        app.getApp().afficherFentreAjouterAthlete();
     }
 
     
